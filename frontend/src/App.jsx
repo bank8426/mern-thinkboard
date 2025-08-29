39545a46d428ba5269ec9b9b42ab0bfd81a81615
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-08-29 17:09:31 +0700

    add routes to each page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreateNotePage from './pages/CreateNotePage'
+import NoteDetailPage from './pages/NoteDetailPage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/create' element={<CreateNotePage />}></Route>
+        <Route path='/note/:id' element={<NoteDetailPage />}></Route>
+      </Routes>
+    </div>
   )
 }
 
